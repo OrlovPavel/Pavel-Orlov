02415d0d1a06a8ea9b47b1cdfb42f6f2afb186aa
OrlovPavel <70897595+OrlovPavel@users.noreply.github.com>
2020-10-19 14:13:37 +0300

    task D

--- main.cpp
+++ main.cpp
@@ -1,89 +1,60 @@
 #include <iostream>
-#include <string>
+#include <deque>
 
-class MyStack{
+class MyQueue {
 private:
-    struct stackCell{
-        int value;
-        stackCell *previous;
-    };
-    stackCell *top;
-    int size;
+    std::deque<int> firstPart;
+    std::deque<int> secondPart;
 public:
-    MyStack(){
-        this->top = nullptr;
-        this->size = 0;
+    void pushBack(int i){
+        this->firstPart.push_back(i);
+        this->balance();
     }
-    bool isEmpty(){
-        if(this->size == 0) {
-            return true;
+    void pushCenter(int i){
+        this->firstPart.push_front(i);
+        this->balance();
+    }
+    void balance(){
+        int firstSize = this->firstPart.size();
+        int secondSize =this->secondPart.size();
+        if(firstSize != secondSize){
+            if(secondSize - firstSize >= 2){
+                this->firstPart.push_front(this->secondPart.back());
+                this->secondPart.pop_back();
+            }
+            if(firstSize > secondSize){
+                this->secondPart.push_back(this->firstPart.front());
+                this->firstPart.pop_front();
+            }
         }
-        return false;
     }
-    void push(int value){
-        stackCell *newTop = new stackCell();
-        newTop->value = value;
-        newTop->previous = top;
-        this->top = newTop;
-        this->size++;
-    }
-    void pop(){
-        if(this->isEmpty()) {
-            return;
-        }
-        stackCell *deletableTop = this->top;
-        this->top = deletableTop->previous;
-        delete deletableTop;
-        this->size--;
-    }
-    int getTopValue(){
-        if(this->isEmpty()) {
-            return -1;
-        }
-        return this->top->value;
-    }
-    int getSize(){
-        return this->size;
-    }
-    void clear(){
-        while(this->size != 0){
-            this->pop();
-        }
+    int pop(){
+        int res = 0;
+        res = this->secondPart.front();
+        this->secondPart.pop_front();
+        this->balance();
+        return res;
     }
 };
 
 int main() {
-    MyStack stack;
-    int value;
-    std::string query;
-    std::cin >> query;
-    while(query != "exit"){
-        if(query == "push"){
-            std::cin >> value;
-            stack.push(value);
-            std::cout << "ok" << std::endl;
+    char query;
+    MyQueue queue;
+    int n, i;
+    std::cin >> n;
+    for(int j = 0; j < n; ++j){
+        std:: cin >> query;
+        if(query == '+'){
+            std::cin >> i;
+            queue.pushBack(i);
         }
-        if(query == "back" || query == "pop"){
-            value = stack.getTopValue();
-            if(stack.getSize() == 0){
-                std::cout << "error" << std::endl;
-            }
-            else{
-                std::cout << value << std:: endl;
-                if(query == "pop"){
-                    stack.pop();
-                }
-            }
+        if(query == '*'){
+            std::cin >> i;
+            queue.pushCenter(i);
         }
-        if(query == "size"){
-            std::cout << stack.getSize() << std::endl;
+        if(query == '-'){
+            std::cout << queue.pop() << std::endl;
         }
-        if(query == "clear"){
-            stack.clear();
-            std::cout << "ok" << std::endl;
-        }
-        std::cin >> query;
     }
-    std::cout << "bye";
     return 0;
 }
