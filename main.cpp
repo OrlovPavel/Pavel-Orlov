f83bdf16e14ef0de13257dcd193af40a36616f44
OrlovPavel <70897595+OrlovPavel@users.noreply.github.com>
2020-11-02 14:16:50 +0300

    task H fixed

--- main.cpp
+++ main.cpp
@@ -1,54 +1,46 @@
 #include <iostream>
 
 typedef unsigned long long lld;
-
-lld pow(lld a, lld n){
-    if(n == 0) {
-        return 1;
-    }
-    if(n % 2 == 1) {
-        return a * pow(a, (n - 1));
-    }
-    return pow(a, n / 2) * pow(a, n / 2);
-}
+const int byte = 256;
 
 void stableSort(lld* a, int n, lld *copy, int *digits, int k){
-    lld p = pow(10, k), q = pow(10, k - 1);
     for(int i = 0; i < n; ++i){
-        digits[(a[i] % p) / q]++;
+        digits[(a[i] >> (8 * (k - 1))) % byte]++;
     }
-    for(int i = 1; i < 10; ++i){
+    for(int i = 1; i < byte; ++i){
         digits[i] += digits[i - 1];
     }
     for(int i = n - 1; i >= 0; --i){
-        copy[--digits[(a[i] % p) / q]] = a[i];
+        copy[--digits[(a[i] >> (8 * (k - 1))) % byte]] = a[i];
     }
     for(int i = n - 1; i >= 0; --i){
         a[i] = copy[i];
         copy[i] = 0;
     }
-    for(int i = 1; i < 10; ++i){
+    for(int i = 1; i < byte; ++i){
         digits[i] = 0;
     }
 }
 
 void LSDsort(lld *a, int n){
-    int *digits = (int*)calloc(10, sizeof(int));// доделай освобождение памяти
-    lld *copy = (lld*)calloc(n, sizeof(lld));// И тут
+    int *digits = (int*)calloc(byte, sizeof(int));
+    lld *copy = (lld*)calloc(n, sizeof(lld));
     int k = 1;
-    while(k < 20){
+    while(k < 9){
         stableSort(a, n, copy, digits, k);
         /*for(int i = 0; i < n; ++i)
             std::cout << a[i] << " ";
         std::cout << "\n";*/
         k++;
     }
+    free(digits);
+    free(copy);
 }
 
 int main() {
     int n;
     scanf("%d", &n);
-    lld *a = (lld*)calloc(n, sizeof(lld));// И тут
+    lld *a = (lld*)calloc(n, sizeof(lld));
     for(int i = 0; i < n; ++i){
         scanf("%llu", &a[i]);
     }
@@ -56,5 +48,6 @@ int main() {
     for(int i = 0; i < n; ++i){
         printf("%llu ", a[i]);
     }
+    free(a);
     return 0;
 }
