9785739bf4e7b7818b1dc07d5073c13271e96b79
OrlovPavel <70897595+OrlovPavel@users.noreply.github.com>
2021-03-29 14:25:33 +0300

    task J

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+    int s, n;
+    std::cin >> s >> n;
+    int* w = new int[n + 1];
+    for(int i = 1; i < n + 1; ++i){
+        std::cin >> w[i];
+    }
+    int** dp = new int*[n + 1];
+    for(int i = 0; i < n + 1; ++i){
+        dp[i] = new int[s + 1];
+        dp[i][0] = 0;
+    }
+    for(int j = 0; j < s + 1; ++j)
+        dp[0][j] = 0;
+    for(int i = 1; i < n + 1; ++i){
+        for(int j = 1; j < s + 1; ++j){
+            dp[i][j] = dp[i - 1][j];
+            if(w[i] <= j && dp[i - 1][j - w[i]] + w[i] > dp[i][j])
+                dp[i][j] = dp[i - 1][j - w[i]] + w[i];
+        }
+    }
+    std::cout << dp[n][s];
+    return 0;
+}
