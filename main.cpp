b9ac9dca8bc9a5ee1a45d19f35c6ab9ff4b95ce7
OrlovPavel <70897595+OrlovPavel@users.noreply.github.com>
2021-04-18 16:32:17 +0300

    v2

--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 
-int main() { // вся функциональность в main
-    int n, m;
+void insert(int& n, int& m, int* a, int* b){
     std::cin >> n;
-    int* a = new int[n]; // выделенная память не освобождается
     for(int i = 0; i < n; ++i)
         std::cin >> a[i];
     std::cin >> m;
-    int* b = new int[m];
     for(int i = 0; i < m; ++i)
         std::cin >> b[i];
-    int** dp = new int*[n];
+}
+
+void fill_dp(int& n, int& m, int* a, int* b, int** dp){
     for(int i = 0; i < n; ++i){
         dp[i] = new int[m];
         for(int j = 0; j < m; ++j){
@@ -23,13 +22,32 @@ int main() { // вся функциональность в main
                     dp[i][j] = 0;
         }
     }
-    for(int i = 1; i < n; ++i)
-        for(int j = 1; j < m; ++j){
-            if(a[i] == b[j])
+    for(int i = 1; i < n; ++i) {
+        for (int j = 1; j < m; ++j) {
+            if (a[i] == b[j])
                 dp[i][j] = dp[i - 1][j - 1] + 1;
             else
                 dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
         }
+    }
+}
+
+void free_memory(int* a, int* b, int& n, int** dp){
+    delete[] a;
+    delete[] b;
+    for(int i = 0; i < n; ++i)
+        delete[] dp[i];
+    delete[] dp;
+}
+
+int main() {
+    int n, m;
+    int* a = new int[1000];
+    int* b = new int[1000];
+    insert(n, m, a, b);
+    int** dp = new int*[n];
+    fill_dp(n, m, a, b, dp);
     std::cout << dp[n - 1][m - 1];
+    free_memory(a, b, n, dp);
     return 0;
 }
